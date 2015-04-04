a9c1a97d04029abd01f2fc31d9652e49a16c09eb
becario92 <fjalva01@gmail.com>
2015-04-04 15:36:02 +0200

    Update BothOpened (hereda de Interval)

--- src/intervals/BothOpened.java
+++ src/intervals/BothOpened.java
@@ -1,5 +1,10 @@
 package intervals;
 
-public class BothOpened {
+public class BothOpened extends Interval {
+
+	public BothOpened(double minimum, double maximum, Opening opening) {
+		super(minimum, maximum, opening);
+		// TODO Auto-generated constructor stub
+	}
 
 }
