7fa72f61ed813309f550582fc62d16f2c32beafd
becario92 <fjalva01@gmail.com>
2015-04-04 15:40:15 +0200

    La clase BothOpened se llama BothOpenedInterval

--- src/intervals/BothOpened.java
+++
@@ -1,9 +0,0 @@
-package intervals;
-
-public class BothOpened extends Interval {
-
-	public BothOpened(double minimum, double maximum, Opening opening) {
-		super(minimum, maximum, opening);
-	}
-
-}
