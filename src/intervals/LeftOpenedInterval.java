cbac799b4e0d0886597a92fa214bb10c68cc42a7
becario92 <fjalva01@gmail.com>
2015-04-04 15:55:46 +0200

    Update LeftOpenedInterval (método includes[value])

--- src/intervals/LeftOpenedInterval.java
+++ src/intervals/LeftOpenedInterval.java
@@ -5,5 +5,9 @@ public class LeftOpenedInterval extends Interval {
 	public LeftOpenedInterval(double minimum, double maximum, Opening opening) {
 		super(minimum, maximum, opening);
 	}
+	
+	public boolean includes(double value) {
+		return getMinimum() < value && value <= getMaximum();
+	}
 
 }
